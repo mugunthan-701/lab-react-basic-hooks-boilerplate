f7c50fd3faef50c250f00c8cbf3542a6be38caef
mugunthan s <mugunthan701@gmail.com>
2023-12-08 17:26:30 +0530

    completed'

--- HooksBasics/vite.config.js
+++ HooksBasics/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],base:"lab-react-basic-hooks-boilerplate"
+  plugins: [react()],base:"/lab-react-basic-hooks-boilerplate"
 })
